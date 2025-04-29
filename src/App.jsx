b776a892ffbbecb98f8beb29c6d975f839b33103
johnson williams <williams.gb55@gmail.com>
2025-04-29 16:42:44 +0900

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import NavBar from "./components/NavBar";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 const App = () => {
   return (
     <>
+      <NavBar />
       <Hero />
       <ShowcaseSection />
     </>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import { navLinks } from "../constants";
+
+const NavBar = () => {
+  const [scrolled, setScrolled] = React.useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 10;
+      setScrolled(true);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
+      <div className="inner">
+        <a className="logo" href="#hero">
+          JSon | DEV
+        </a>
+        <nav className="desktop">
+          <ul>
+            {navLinks.map(({ link, name }) => (
+              <li key={name} className="group">
+                <a href={link}>
+                  <span>{name}</span>
+                  <span className="underline" />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <a href="#contact" className="contact-btn group">
+          <div className="inner">
+            <span>Contact me</span>
+          </div>
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
